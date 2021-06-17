e5f2ab53619c72db96f55490f0d655caed2814db
researcher2 <32028883+researcher2@users.noreply.github.com>
2021-06-17 18:56:22 +1000

    Update basic_example.py

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,9 +1,15 @@
 from best_download import download_file
 
+tests = []
+tests.append(("http://speedcheck.cdn.on.net/100meg.test", "100meg.test",
+    "b89222456931da603c4c601208b6bf9d1a0d7cad3bde957163612074b04e6154"))
+tests.append(("http://speedcheck.cdn.on.net/1000meg.test", "1000meg.test",
+    "cfab8f3761126268a6715f90796a68074c3f57c3af48e0101776d211e7b5139e"))
+
+print(tests)
+
 def main():
-    url = "http://speedcheck.cdn.on.net/1000meg.test"
-    local_file_path = "1000meg.test"
-    checksum = "cfab8f3761126268a6715f90796a68074c3f57c3af48e0101776d211e7b5139e"
+    url, local_file_path, checksum = tests[0]
 
     print(f"Testing download of file {url} to {local_file_path}")
     print("Please cancel half way through and re-run this example to test resuming")
